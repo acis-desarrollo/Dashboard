297a91515e618b06cd5aef968eb3a2b94908db53
acis-desarrollo <nelson.huaman@acis.edu.pe>
2025-09-09 10:48:49 -0500

    Fix ESLint version conflict and Next.js config file

---
+++ next.config.js
@@ -0,0 +1,10 @@
+@@ .. @@
+-import type { NextConfig } from "next";
+-
+-const nextConfig: NextConfig = {
++const nextConfig = {
+   /* config options here */
+ };
+ 
+-export default nextConfig;
++module.exports = nextConfig;
