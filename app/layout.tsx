72cca989a898a1251beb596464fcaef37bb976e6
acis-desarrollo <nelson.huaman@acis.edu.pe>
2025-09-13 10:05:18 -0500

    Updated layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
    return (
       <html lang="en">
          <body className={roboto.className}>
-            {children}
+           <DashboardLayout>{children}</DashboardLayout>
          </body>
       </html>
    );
